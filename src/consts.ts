77d85ecc2c239d0bb936bcf975999da5fef84af4
mtonc <m.tonchick@gmail.com>
2025-01-17 16:44:37 -0600

    First Commit for portfolio site

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'mtonc.dev';
+export const SITE_DESCRIPTION = 'The ramblings of a web developer';
---
+++ src/store.js
@@ -0,0 +1,3 @@
+import { atom } from 'nanostores';
+
+export const $color = atom('4af626');
